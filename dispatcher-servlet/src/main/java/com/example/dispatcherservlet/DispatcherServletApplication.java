3129f476bcfb9b84ab55b8751756547074d14737
yhnoh <tedgom19@gamil.com>
2022-03-11 23:12:40 +0900

    dispatcher-servlet

---
+++ dispatcher-servlet/src/main/java/com/example/dispatcherservlet/DispatcherServletApplication.java
@@ -0,0 +1,13 @@
+package com.example.dispatcherservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispatcherServletApplication.class, args);
+    }
+
+}
---
+++ dispatcher-servlet/src/test/java/com/example/dispatcherservlet/DispatcherServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dispatcherservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
